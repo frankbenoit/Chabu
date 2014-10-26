f8e54658cba0337042e1d38567fa27fa83413d33
Frank Benoit <frank.rene.benoit@gmail.com>
2014-10-26 21:09:15 +0100

    refactor decoding

--- java/ChabuTest/src/chabu/tester/data/ACommand.java
+++ java/ChabuTest/src/chabu/tester/data/ACommand.java
@@ -1,5 +1,10 @@
 package chabu.tester.data;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+import chabu.Utils;
+
 
 public abstract class ACommand {
 	
@@ -8,4 +13,94 @@ public abstract class ACommand {
 	}
 	
 	public final CommandId commandId;
+	
+	
+	
+	public static ACommand decodeCommand(ByteBuffer buf) {
+		int cmd = buf.get(0);
+		if( cmd == CommandId.TIME_BROADCAST.getId() ){
+			return cmdTimeBroadcast   ( buf );
+		}
+		else if( cmd == CommandId.APPLICATION_CLOSE.getId() ){
+			return cmdCloseApplication( buf );
+		}
+		else if( cmd == CommandId.CONNECTION_CLOSE.getId() ){
+			return cmdCloseConnection ( buf );
+		}
+		else if( cmd == CommandId.CONNECTION_CONNECT.getId() ){
+			return cmdStartConnection ( buf );
+		}
+		else if( cmd == CommandId.CONNECTION_AWAIT.getId() ){
+			return cmdAwaitConnection ( buf );
+		}
+		else if( cmd == CommandId.CHANNEL_ADD.getId() ){
+			return cmAddChannel   ( buf );
+		}
+		else if( cmd == CommandId.CHANNEL_ACTION.getId() ){
+			return cmdActionChannel   ( buf );
+		}
+		else if( cmd == CommandId.CHANNEL_CREATE_STAT.getId() ){
+			return cmdCreateChannelStat   ( buf );
+		}
+		else {
+			throw Utils.fail( "unknown command id %d", cmd );
+		}
+	}
+
+	private static ACommand cmdCreateChannelStat(ByteBuffer buf) {
+		long time      = buf.getLong();
+		int  channelId = buf.get() & 0xFF;
+		return new CmdChannelCreateStat(time, channelId );
+	}
+
+	private static ACommand cmAddChannel(ByteBuffer buf) {
+		long time      = buf.getLong();
+		int  channelId = buf.get() & 0xFF;
+		int  rxCount   = buf.getInt();
+		return new CmdChannelAdd(time, channelId, rxCount );
+	}
+
+	private static ACommand cmdActionChannel(ByteBuffer buf) {
+		long time     = buf.getLong();
+		int channelId = buf.get() & 0xFFFF;
+		int txCount   = buf.getInt();
+		int rxCount   = buf.getInt();
+		return new CmdChannelAction(time, channelId, txCount, rxCount );
+	}
+
+	private static ACommand cmdCloseConnection(ByteBuffer buf) {
+		long time = buf.getLong();
+		return new CmdConnectionClose(time);
+	}
+
+	private static ACommand cmdStartConnection(ByteBuffer buf) {
+		long   time    = buf.getLong();
+		String address = decodeString( buf );
+		int    port    = buf.getShort();
+		return new CmdConnectionConnect(time, address, port );
+	}
+
+	private static ACommand cmdAwaitConnection(ByteBuffer buf) {
+		long time = buf.getLong();
+		int  port = buf.getShort();
+		return new CmdConnectionAwait( time, port );
+	}
+
+	private static ACommand cmdCloseApplication(ByteBuffer buf) {
+		long time = buf.getLong();
+		return new CmdApplicationClose( time );
+	}
+
+	private static ACommand cmdTimeBroadcast(ByteBuffer buf) {
+		long time = buf.getLong();
+		return new CmdTimeBroadcast( time );
+	}
+
+	private static String decodeString( ByteBuffer bb ){
+		int len = bb.getShort();
+		byte[] data = new byte[len];
+		bb.get( data );
+		return new String( data, StandardCharsets.UTF_8 );
+	}
+	
 }
--- java/ChabuTest/src/chabu/tester/dut/ControlConnection.java
+++ java/ChabuTest/src/chabu/tester/dut/ControlConnection.java
@@ -2,21 +2,11 @@ package chabu.tester.dut;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.nio.charset.StandardCharsets;
 
 import chabu.INetwork;
 import chabu.INetworkUser;
 import chabu.Utils;
 import chabu.tester.data.ACommand;
-import chabu.tester.data.CmdApplicationClose;
-import chabu.tester.data.CmdChannelAction;
-import chabu.tester.data.CmdChannelAdd;
-import chabu.tester.data.CmdChannelCreateStat;
-import chabu.tester.data.CmdConnectionAwait;
-import chabu.tester.data.CmdConnectionClose;
-import chabu.tester.data.CmdConnectionConnect;
-import chabu.tester.data.CmdTimeBroadcast;
-import chabu.tester.data.CommandId;
 
 public class ControlConnection implements INetworkUser {
 
@@ -43,102 +33,14 @@ public class ControlConnection implements INetworkUser {
 				return;
 			}
 			int startPos = buf.position();
-			int cmd = buf.get(0);
-			if( cmd == CommandId.TIME_BROADCAST.getId() ){
-				cmdTimeBroadcast   ( buf );
-			}
-			else if( cmd == CommandId.APPLICATION_CLOSE.getId() ){
-				cmdCloseApplication( buf );
-			}
-			else if( cmd == CommandId.CONNECTION_CLOSE.getId() ){
-				cmdCloseConnection ( buf );
-			}
-			else if( cmd == CommandId.CONNECTION_CONNECT.getId() ){
-				cmdStartConnection ( buf );
-			}
-			else if( cmd == CommandId.CONNECTION_AWAIT.getId() ){
-				cmdAwaitConnection ( buf );
-			}
-			else if( cmd == CommandId.CHANNEL_ADD.getId() ){
-				cmAddChannel   ( buf );
-			}
-			else if( cmd == CommandId.CHANNEL_ACTION.getId() ){
-				cmdActionChannel   ( buf );
-			}
-			else if( cmd == CommandId.CHANNEL_CREATE_STAT.getId() ){
-				cmdCreateChannelStat   ( buf );
-			}
-			else {
-				Utils.ensure( false, "unknown command id %d", cmd );
-			}
+			ACommand cmd = ACommand.decodeCommand(buf);
 			int endPos = buf.position();
 			Utils.ensure( endPos - startPos == len );
+			scheduler.addCommand(cmd);
 		}
 		
 	}
 
-	private void cmdCreateChannelStat(ByteBuffer buf) {
-		long time      = buf.getLong();
-		int  channelId = buf.get() & 0xFF;
-		ACommand cmd = new CmdChannelCreateStat(time, channelId );
-		scheduler.addCommand(cmd);
-	}
-
-	private void cmAddChannel(ByteBuffer buf) {
-		long time      = buf.getLong();
-		int  channelId = buf.get() & 0xFF;
-		int  rxCount   = buf.getInt();
-		ACommand cmd = new CmdChannelAdd(time, channelId, rxCount );
-		scheduler.addCommand(cmd);
-	}
-
-	private void cmdActionChannel(ByteBuffer buf) {
-		long time     = buf.getLong();
-		int channelId = buf.get() & 0xFFFF;
-		int txCount   = buf.getInt();
-		int rxCount   = buf.getInt();
-		ACommand cmd = new CmdChannelAction(time, channelId, txCount, rxCount );
-		scheduler.addCommand(cmd);
-	}
-
-	private void cmdCloseConnection(ByteBuffer buf) {
-		long time = buf.getLong();
-		ACommand cmd = new CmdConnectionClose(time);
-		scheduler.addCommand(cmd);
-	}
-
-	private void cmdStartConnection(ByteBuffer buf) {
-		long   time    = buf.getLong();
-		String address = extractString( buf );
-		int    port    = buf.getShort();
-		ACommand cmd = new CmdConnectionConnect(time, address, port );
-		scheduler.addCommand(cmd);
-	}
-
-	private String extractString( ByteBuffer bb ){
-		int len = bb.getShort();
-		byte[] data = new byte[len];
-		bb.get( data );
-		return new String( data, StandardCharsets.UTF_8 );
-	}
-	private void cmdAwaitConnection(ByteBuffer buf) {
-		long time = buf.getLong();
-		int  port = buf.getShort();
-		ACommand cmd = new CmdConnectionAwait( time, port );
-		scheduler.addCommand(cmd);
-	}
-
-	private void cmdCloseApplication(ByteBuffer buf) {
-		long time = buf.getLong();
-		ACommand cmd = new CmdApplicationClose( time );
-		scheduler.addCommand(cmd);
-	}
-
-	private void cmdTimeBroadcast(ByteBuffer buf) {
-		long time = buf.getLong();
-		ACommand cmd = new CmdTimeBroadcast( time );
-		scheduler.addCommand(cmd);
-	}
 
 	@Override
 	public void evXmit(ByteBuffer bufferToFill) {

7b3783e5464852e4320dd52f125a10322ab39308
Frank Benoit <frank.rene.benoit@gmail.com>
2014-10-29 20:07:35 +0100

    Fix encoding/decoding

--- java/ChabuTest/src/chabu/tester/data/ACommand.java
+++ java/ChabuTest/src/chabu/tester/data/ACommand.java
@@ -18,40 +18,66 @@ public abstract class ACommand {
 	
 	public static void encodeCommand( ByteBuffer buf, ACommand cmd ){
 		int pos = buf.position();
-		buf.putInt(0); // fill dummy length
+		buf.putInt(123); // fill dummy length
 		cmd.encode(buf);
-		buf.putInt( pos, buf.position() - pos - 4 ); // now make the length valid
+		int packetSz = buf.position() - pos - 4;
+		System.out.printf("ACommand.encode packetSz:%s %s\n", packetSz, buf);
+		buf.putInt( pos, packetSz ); // now make the length valid
 	}
 	
 	public static ACommand decodeCommand(ByteBuffer buf) {
-		int cmd = buf.get(0);
-		if( cmd == CommandId.TIME_BROADCAST.getId() ){
-			return cmdTimeBroadcast   ( buf );
+		System.out.printf("ACommand.decode %s\n", buf);
+		if( buf.remaining() < 4 ) return null;
+		
+		int pos = buf.position();
+		int packetSz = buf.getInt();
+		
+		System.out.printf("ACommand.decode pos:%s sz:%s\n", pos, packetSz);
+		if( buf.remaining() < packetSz ){
+			buf.position(pos);
+			return null;
 		}
-		else if( cmd == CommandId.DUT_APPLICATION_CLOSE.getId() ){
-			return cmdCloseApplication( buf );
+		
+		ACommand res = null;
+		try{
+			int cmd = buf.get();
+			if( cmd == CommandId.TIME_BROADCAST.getId() ){
+				res = cmdTimeBroadcast   ( buf );
+			}
+			else if( cmd == CommandId.DUT_APPLICATION_CLOSE.getId() ){
+				res = cmdCloseApplication( buf );
+			}
+			else if( cmd == CommandId.CONNECTION_CLOSE.getId() ){
+				res = cmdCloseConnection ( buf );
+			}
+			else if( cmd == CommandId.CONNECTION_CONNECT.getId() ){
+				res = cmdStartConnection ( buf );
+			}
+			else if( cmd == CommandId.CONNECTION_AWAIT.getId() ){
+				res = cmdAwaitConnection ( buf );
+			}
+			else if( cmd == CommandId.CHANNEL_ADD.getId() ){
+				res = cmAddChannel   ( buf );
+			}
+			else if( cmd == CommandId.CHANNEL_ACTION.getId() ){
+				res = cmdActionChannel   ( buf );
+			}
+			else if( cmd == CommandId.CHANNEL_CREATE_STAT.getId() ){
+				res = cmdCreateChannelStat   ( buf );
+			}
+			else {
+				throw Utils.fail( "unknown command id %d", cmd );
+			}
 		}
-		else if( cmd == CommandId.CONNECTION_CLOSE.getId() ){
-			return cmdCloseConnection ( buf );
+		finally{
+			if( res != null ){
+				Utils.ensure( pos + packetSz + 4 == buf.position(), "%d %d +4 == %s", pos, packetSz, buf );
+			}
+			else {
+				buf.position(pos);
+			}
 		}
-		else if( cmd == CommandId.CONNECTION_CONNECT.getId() ){
-			return cmdStartConnection ( buf );
-		}
-		else if( cmd == CommandId.CONNECTION_AWAIT.getId() ){
-			return cmdAwaitConnection ( buf );
-		}
-		else if( cmd == CommandId.CHANNEL_ADD.getId() ){
-			return cmAddChannel   ( buf );
-		}
-		else if( cmd == CommandId.CHANNEL_ACTION.getId() ){
-			return cmdActionChannel   ( buf );
-		}
-		else if( cmd == CommandId.CHANNEL_CREATE_STAT.getId() ){
-			return cmdCreateChannelStat   ( buf );
-		}
-		else {
-			throw Utils.fail( "unknown command id %d", cmd );
-		}
+		return res;
 	}
 
 	private static ACommand cmdCreateChannelStat(ByteBuffer buf) {

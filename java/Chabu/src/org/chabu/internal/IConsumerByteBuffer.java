67e4e68a78991aede2cd3fcc4767cb1710a90c97
Frank Benoit <keinfarbton@gmail.com>
2015-06-28 13:34:15 +0200

    Add missing files.

---
+++ java/Chabu/src/org/chabu/internal/IConsumerByteBuffer.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * Copyright (c) 2015 Frank Benoit.
+ *
+ * See the LICENSE.md or the online documentation:
+ * https://docs.google.com/document/d/1Wqa8rDi0QYcqcf0oecD8GW53nMVXj3ZFSmcF81zAa8g/edit#heading=h.2kvlhpr5zi2u
+ *
+ * Contributors:
+ *     Frank Benoit - initial API and implementation
+ *******************************************************************************/
+package org.chabu.internal;
+
+import java.nio.ByteBuffer;
+
+/**
+ * A replacement for Java8 <code>Consumer&lt;ByteBuffer&gt;</code>
+ */
+public interface IConsumerByteBuffer {
+
+    public void accept( ByteBuffer buffer );
+
+}

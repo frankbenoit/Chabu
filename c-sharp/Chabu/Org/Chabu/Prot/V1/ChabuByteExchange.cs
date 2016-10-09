38f9712f281ebf854dbee9043f0d7f3615a4f927
Frank Benoit <fr@nk-benoit.de>
2016-10-10 00:24:08 +0200

    Fix email

--- c-sharp/Chabu/Org/Chabu/Prot/V1/ChabuByteExchange.cs
+++ c-sharp/Chabu/Org/Chabu/Prot/V1/ChabuByteExchange.cs
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * The MIT License (MIT)
- * Copyright (c) 2015 Frank Benoit, Stuttgart, Germany <keinfarbton@gmail.com>
+ * Copyright (c) 2015 Frank Benoit, Stuttgart, Germany <fr@nk-benoit.de>
  * 
  * See the LICENSE.md or the online documentation:
  * https://docs.google.com/document/d/1Wqa8rDi0QYcqcf0oecD8GW53nMVXj3ZFSmcF81zAa8g/edit#heading=h.2kvlhpr5zi2u
